cfa9090cad7e457d6b40025552ec504d63ca0aca
Intouch Yutinatorn <keyeducation2544@gmail.com>
2021-02-16 23:01:12 +0700

    Add cors

--- server.js
+++ server.js
@@ -3,61 +3,60 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+
 // app.get("/", function (req, res) {
 //   res.send("Hello world\n");
 // });
 
 // app.listen(PORT);
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 //const postparser = require('./middleware/postParser');
-const Todo = require('./todo');
+const Todo = require("./todo");
 
 app.use(bodyParser.json());
 
-app.get('/', function(request, response){
+app.get("/", function (request, response) {
   Todo.all((err, todos) => response.status(200).json(todos));
 });
 
-app.get('/done', function(request, response){
+app.get("/done", function (request, response) {
   Todo.done((err, todos) => response.status(200).json(todos));
-  
 });
 
-app.get('/notdone', function(request, response){
+app.get("/notdone", function (request, response) {
   Todo.notdone((err, todos) => response.status(200).json(todos));
-  
 });
 
-app.post('/', (request, response) => {
-    console.log(request.body);
-    var newTodo = request.body;
-    Todo.add(newTodo);
-    response.status(201).json();
+app.post("/", (request, response) => {
+  console.log(request.body);
+  var newTodo = request.body;
+  Todo.add(newTodo);
+  response.status(201).json();
 });
 
-app.put('/:id', (request, response) => {
-  console.log(request.body); 
+app.put("/:id", (request, response) => {
+  console.log(request.body);
   var id = parseInt(request.params.id);
   var updatedTodo = request.body;
   updatedTodo.id = id;
   Todo.update(updatedTodo, (err, data) => {
-    if(err)
-    {
-      response.status(404, 'The task is not found').send();
+    if (err) {
+      response.status(404, "The task is not found").send();
     } else {
-    response.status(204).send(data);
-  }
-});
+      response.status(204).send(data);
+    }
+  });
 });
 
-app.delete('/:id', (request, response) => {
+app.delete("/:id", (request, response) => {
   var id = parseInt(request.params.id);
   Todo.delete(id, (err) => {
-    if(err){
+    if (err) {
       response.status(404).send();
-    }else{
-          response.status(200).send();
+    } else {
+      response.status(200).send();
     }
   });
 });
